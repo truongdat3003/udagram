ae72e7a959adf5840b2ec514010303894a7132b0
truongdat3003 <truongdat3003@gmail.com>
2023-11-05 23:42:38 +0700

    update

--- udagram/set_env.sh
+++ udagram/set_env.sh
@@ -1,14 +1,11 @@
 # This file is used for convenience of local development.
 # DO NOT STORE YOUR CREDENTIALS INTO GIT
-eb setenv POSTGRES_PASSWORD=$POSTGRES_PASSWORD	
-eb setenv POSTGRES_HOST=$POSTGRES_HOST
-eb setenv POSTGRES_DB=$POSTGRES_DB
-eb setenv AWS_BUCKET=$AWS_BUCKET
-eb setenv S3_BUCKET_FRONTEND= $S3_BUCKET_FRONTEND
-eb setenv AWS_REGION=$AWS_DEFAULT_REGION
-eb setenv AWS_PROFILE=$AWS_PROFILE
-eb setenv JWT_SECRET=$JWT_SECRET
-eb setenv URL=http://udagram-api-dev.us-east-1.elasticbeanstalk.com/
-eb setenv AWS_DEFAULT_REGION=$AWS_DEFAULT_REGION
-eb setenv profile=$profile
-eb setenv PORT=$PORT USERNAME=$USERNAME
+export POSTGRES_USERNAME=postgres
+export POSTGRES_PASSWORD=postgres
+export POSTGRES_HOST=udagramdb.cpmbomf1vmkr.us-east-1.rds.amazonaws.com
+export POSTGRES_DB=postgres
+export AWS_BUCKET=arn:aws:s3:::udagram-fe
+export AWS_REGION=us-east-1
+export AWS_PROFILE=default
+export JWT_SECRET=hello
+export URL=http://localhost:8080
